23c336fe5f807f0ef797afb5785595559bce5e44
Alejogm2022 <alejandro.gomezmeneses@gmail.com>
2024-11-08 19:04:18 -0500

    configuracion-jdk

--- extra-hours-amadeus/src/main/java/com/example/extra_hours_amadeus/ExtraHoursAmadeusApplication.java
+++ extra-hours-amadeus/src/main/java/com/example/extra_hours_amadeus/ExtraHoursAmadeusApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ExtraHoursAmadeusApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ExtraHoursAmadeusApplication.class, args);
+	public static void main(String[] args) {SpringApplication.run(ExtraHoursAmadeusApplication.class, args);
 	}
 
 }
